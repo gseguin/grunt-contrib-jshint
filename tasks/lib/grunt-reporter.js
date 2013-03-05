530d965d2ba7fa6b2d8ff93c99c7564901236973
Ghislain Seguin <ghislain@gmail.com>
2013-03-05 16:51:06 -0700

    Fixed default reporter ( it wasn't printing the filename )

--- tasks/lib/grunt-reporter.js
+++ tasks/lib/grunt-reporter.js
@@ -9,6 +9,7 @@ exports.init = function(grunt) {
 
 	function defaultReporter(results, data) {
 		var out = '';
+        var file;
 		results.forEach( function(w) {
 			var e = w.error;
 			// Sometimes there's no error object.
@@ -16,12 +17,16 @@ exports.init = function(grunt) {
 			var pos;
 			var evidence = e.evidence;
 			var character = e.character;
+            if ( file != w.file ) {
+                file = w.file;
+                out = 'Linting ' + file + '...' + 'ERROR'.red + '\n';
+            }
 			if (evidence) {
 				// Manually increment errorcount since we're not using grunt.log.error().
 				grunt.fail.errorcount++;
 				// Descriptive code error.
 				pos = '['.red + ('L' + e.line).yellow + ':'.red + ('C' + character).yellow + ']'.red;
-				out = pos + ' ' + e.reason.yellow + '\n';
+				out += pos + ' ' + e.reason.yellow + '\n';
 				if (character === 0) {
 					// Beginning of line.
 					evidence = '?'.inverse.red + evidence;
