91392f664eb57d27e4d67cbc23d3e09afc35418a
Ghislain Seguin <ghislain@gmail.com>
2013-03-08 07:56:56 -0700

    API changed, adapted test

--- test/jshint_test.js
+++ test/jshint_test.js
@@ -8,7 +8,7 @@ exports['jshint'] = function(test) {
   grunt.log.muted = true;
 
   test.doesNotThrow(function() {
-    jshint.lint(grunt.file.read('test/fixtures/lint.txt'));
+    jshint.hint( [ 'test/fixtures/lint.txt' ] );
   }, 'It should not blow up if an error occurs on character 0.');
 
   test.done();
